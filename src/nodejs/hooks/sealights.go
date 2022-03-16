5a3dc2840362c83c736b390795e6f8c19a6b3b70
victorkatsovsky <victor.katsovsky@sealights.io>
2022-03-16 21:12:20 +0200

    web cmd line

--- src/nodejs/hooks/sealights.go
+++ src/nodejs/hooks/sealights.go
@@ -1,11 +1,16 @@
 package hooks
 
 import (
+	"fmt"
 	"github.com/cloudfoundry/libbuildpack"
+	"github.com/pkg/errors"
 	"io/ioutil"
 	"os"
-	"path/filepath"
+	"strings"
 )
+
+//const sl_cli_template = "./node_modules/.bin/slnodejs run --token  <token> --buildsessionid <id>  --useinitialcolor true  --proxy <proxy> — index.js"
+const sl_cli_template = " ./node_modules/.bin/slnodejs run "
 
 type SealightsHook struct {
 	libbuildpack.DefaultHook
@@ -25,34 +30,53 @@ func init() {
 func (sl *SealightsHook) AfterCompile(stager *libbuildpack.Stager) error {
 	sl.Log.Info("Hello from Sealights hook")
 
-	//token := os.Getenv("SL_TOKEN")
-	//bsid := os.Getenv("SL_BUILD_SESSION_ID")
-	//proxy := os.Getenv("SL_PROXY")
-	//if token == "" {
-	//	return fmt.Errorf("token cannot be empty (env SL_TOKEN)")
-	//}
-	//if bsid == "" {
-	//	return fmt.Errorf("token cannot be empty (env SL_BUILD_SESSION_ID)")
-	//}
-	//
-	//fmt.Println(proxy)
+	token := os.Getenv("SL_TOKEN")
+	bsid := os.Getenv("SL_BUILD_SESSION_ID")
+	proxy := os.Getenv("SL_PROXY")
+	if token == "" {
+		sl.Log.Error("token cannot be empty (env SL_TOKEN)")
+		return errors.New("empty token")
+	}
+	if bsid == "" {
+		sl.Log.Error("token cannot be empty (env SL_BUILD_SESSION_ID)")
+		return errors.New("empty bsid")
+	}
 
-	p, err := ioutil.ReadFile(filepath.Join(stager.BuildDir(), "package.json"))
+	fmt.Println(proxy)
+
+	bytes, err := ioutil.ReadFile("Procfile")
 	if err != nil {
-		sl.Log.Error("failed to load package json from %s", stager.BuildDir())
-
+		sl.Log.Error("failed to read Procfile")
 		return err
 	}
 
-	stager.Logger().Info(string(p))
+	splitted := strings.Split(string(bytes), " ")
+	web := splitted[0]
+	app := splitted[2]
+
+	fmt.Println("web [%s]", " app [%s]", web, app)
+
+	final := web + sl_cli_template + fmt.Sprintf(" --token %s --buildsessionid %s ", token, bsid)
+	if proxy != "" {
+		final += fmt.Sprintf(" --proxy %s ", proxy)
+	}
+
+	final += " --useinitialcolor true "
+	final += app
+
+	err = ioutil.WriteFile("Procfile", []byte(final), 0700)
+	if err != nil {
+		sl.Log.Error("failed to write Procfile, error: %s", err)
+		return err
+	}
 
 	err = sl.Command.Execute(stager.BuildDir(), os.Stdout, os.Stderr, "npm", "install", "slnodejs")
 	if err != nil {
-		sl.Log.Error("npm install failed with error: " + err.Error())
+		sl.Log.Error("npm install slnodejs failed with error: " + err.Error())
 
 		return err
 	} else {
-		sl.Log.Error("npm install finished sucessfully")
+		sl.Log.Info("npm install slnodejs finished successfully")
 	}
 	return nil
 }
