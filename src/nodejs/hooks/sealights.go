8b00c8603fa624bd0d78c4802abe8390e8eb286b
victorkatsovsky <victor.katsovsky@sealights.io>
2022-03-20 17:52:20 +0200

    refactoring, added test

--- src/nodejs/hooks/sealights.go
+++ src/nodejs/hooks/sealights.go
@@ -9,13 +9,12 @@ import (
 	"strings"
 )
 
-//const sl_cli_template = "./node_modules/.bin/slnodejs run --token  <token> --buildsessionid <id>  --useinitialcolor true  --proxy <proxy> — index.js"
-const sl_cli_template = " node ./node_modules/.bin/slnodejs run "
-
 type SealightsHook struct {
 	libbuildpack.DefaultHook
-	Log     *libbuildpack.Logger
-	Command *libbuildpack.Command
+	Log             *libbuildpack.Logger
+	Command         *libbuildpack.Command
+	EmptyTokenError string `default:"token cannot be empty (env SL_TOKEN)"`
+	EmptyBuildError string `default:"build session id cannot be empty (env SL_BUILD_SESSION_ID)"`
 }
 
 func init() {
@@ -34,12 +33,17 @@ func (sl *SealightsHook) AfterCompile(stager *libbuildpack.Stager) error {
 	bsid := os.Getenv("SL_BUILD_SESSION_ID")
 	proxy := os.Getenv("SL_PROXY")
 	if token == "" {
-		sl.Log.Error("token cannot be empty (env SL_TOKEN)")
-		return fmt.Errorf("token cannot be empty (env SL_TOKEN)")
+		sl.Log.Error(sl.EmptyTokenError)
+		return fmt.Errorf(sl.EmptyTokenError)
 	}
 	if bsid == "" {
-		sl.Log.Error("build session id cannot be empty (env SL_BUILD_SESSION_ID)")
-		return fmt.Errorf("build session id cannot be empty (env SL_BUILD_SESSION_ID)")
+		sl.Log.Error(sl.EmptyBuildError)
+		return fmt.Errorf(sl.EmptyBuildError)
+	}
+
+	err := sl.installAgent(stager)
+	if err != nil {
+		return err
 	}
 
 	bytes, err := ioutil.ReadFile(filepath.Join(stager.BuildDir(), "Procfile"))
@@ -48,9 +52,33 @@ func (sl *SealightsHook) AfterCompile(stager *libbuildpack.Stager) error {
 		return err
 	}
 
-	splitted := strings.Split(string(bytes), " ")
-	app := splitted[2]
+	split := strings.Split(string(bytes), " ")
+	app := split[2]
 
+	newCmd := sl.createAppStartCommandLine(app, token, bsid, proxy)
+
+	sl.Log.Debug("new command line: %s", newCmd)
+
+	err = ioutil.WriteFile(filepath.Join(stager.BuildDir(), "Procfile"), []byte(newCmd), 0755)
+	if err != nil {
+		sl.Log.Error("failed to update Procfile, error: %s", err)
+		return err
+	}
+
+	return nil
+}
+
+func (sl *SealightsHook) installAgent(stager *libbuildpack.Stager) error {
+	err := sl.Command.Execute(stager.BuildDir(), os.Stdout, os.Stderr, "npm", "install", "slnodejs")
+	if err != nil {
+		sl.Log.Error("npm install slnodejs failed with error: " + err.Error())
+		return err
+	}
+	sl.Log.Info("npm install slnodejs finished successfully")
+	return nil
+}
+
+func (sl *SealightsHook) createAppStartCommandLine(app, token, bsid, proxy string) string {
 	var sb strings.Builder
 	sb.WriteString(fmt.Sprintf("web: node ./node_modules/.bin/slnodejs run  --useinitialcolor true --token %s --buildsessionid %s ", token, bsid))
 
@@ -59,22 +87,5 @@ func (sl *SealightsHook) AfterCompile(stager *libbuildpack.Stager) error {
 	}
 
 	sb.WriteString(fmt.Sprintf(" %s", app))
-
-	sl.Log.Debug("new command line: %s", sb.String())
-
-	err = ioutil.WriteFile(filepath.Join(stager.BuildDir(), "Procfile"), []byte(sb.String()), 0755)
-	if err != nil {
-		sl.Log.Error("failed to update Procfile, error: %s", err)
-		return err
-	}
-
-	err = sl.Command.Execute(stager.BuildDir(), os.Stdout, os.Stderr, "npm", "install", "slnodejs")
-	if err != nil {
-		sl.Log.Error("npm install slnodejs failed with error: " + err.Error())
-
-		return err
-	} else {
-		sl.Log.Info("npm install slnodejs finished successfully")
-	}
-	return nil
+	return sb.String()
 }
